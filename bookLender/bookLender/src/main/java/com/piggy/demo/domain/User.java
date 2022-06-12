c9824b7057da922ba771e46221251138e1cfbe53
Icarus-E <105259170+Icarus-E@users.noreply.github.com>
2022-06-12 09:44:08 +0800

    Add files via upload

---
+++ bookLender/bookLender/src/main/java/com/piggy/demo/domain/User.java
@@ -0,0 +1,11 @@
+package com.piggy.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String name;
+}
